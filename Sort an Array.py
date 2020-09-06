3d0a5447d3fd9de08bb78f643e57c432b9550f57
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-06 16:59:27 +0800

    Add files via upload

---
+++ Sort an Array.py
@@ -0,0 +1,9 @@
+#Sort an Array
+
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return sorted(nums)
