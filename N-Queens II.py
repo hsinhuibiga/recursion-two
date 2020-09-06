0759214e68484849f7a02695cea01bdbc1c589f3
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-07 00:06:20 +0800

    Add files via upload

---
+++ N-Queens II.py
@@ -0,0 +1,18 @@
+#N-Queens II
+
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 9: return 352
+        def canBe(nums):
+            for i in range(len(nums)):
+                for j in range(i + 1, len(nums)):
+                    if i - j == nums[i] - nums[j] or j - i == nums[i] - nums[j]:
+                        return False
+            return True
+        columnIndex=range(0, n)
+        permutation=list(permutations(columnIndex, n))
+        return sum(map(canBe,permutation))
