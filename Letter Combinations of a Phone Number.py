3b7b47741e7d1e940536ca6354688e712c3fc91b
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-08 12:29:15 +0800

    Add files via upload

---
+++ Letter Combinations of a Phone Number.py
@@ -0,0 +1,20 @@
+#Letter Combinations of a Phone Number
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        kvmaps = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        res = []
+        self.dfs(digits, 0, res, '', kvmaps)
+        return res
+
+    def dfs(self, string, index, res, path, kvmaps):
+        if index == len(string):
+            if path != '':
+                res.append(path)
+            return
+        for j in kvmaps[string[index]]:
+            self.dfs(string, index + 1, res, path + j, kvmaps)
